4db451d3f2d5a04bd2a3c688565445acb0932c4e
Barış Demirci <baris19052003@gmail.com>
2025-01-24 02:59:52 +0300

    Initial Commit

---
+++ instagram-clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram-clone
+//
+//  Created by Barış Demirci on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

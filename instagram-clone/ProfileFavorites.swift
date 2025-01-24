fa12cddd3f07af09c2db277d9a72bad803dfb654
Barış Demirci <baris19052003@gmail.com>
2025-01-24 03:11:07 +0300

    Profile favorite scrollbar added.

---
+++ instagram-clone/ProfileFavorites.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileFavorites.swift
+//  instagram-clone
+//
+//  Created by Barış Demirci on 24.01.2025.
+//
+
+import SwiftUI
+
+struct ProfileFavorites: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileFavorites()
+}

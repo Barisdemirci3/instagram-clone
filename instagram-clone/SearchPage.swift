5a122d07e573f0d69f2ad233185df550b97f8098
Barış Demirci <baris19052003@gmail.com>
2025-01-26 01:17:14 +0300

    Search Page completed.

---
+++ instagram-clone/SearchPage.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPage.swift
+//  instagram-clone
+//
+//  Created by Barış Demirci on 26.01.2025.
+//
+
+import SwiftUI
+
+struct SearchPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPage()
+}
---
+++ instagram-clone/SearchPageTopBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPageTopBar.swift
+//  instagram-clone
+//
+//  Created by Barış Demirci on 26.01.2025.
+//
+
+import SwiftUI
+
+struct SearchPageTopBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPageTopBar()
+}
